01fdfb1881aff104876adb33b5a135695a1731f6
Rahul Sharma <rahulsharma3258work@gmail.com>
2025-11-11 17:34:44 +0530

    2 :: Even or Odd using if condition

---
+++ 2-Number Even or Odd/Even or Odd using if condition.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    //Variables to store data
+    int num = 3;
+
+    // Logic to check even or odd number using if condition
+    if (num % 2 == 0) {
+        cout << num << " is Even" << endl;
+    } else {
+        cout << num << " is Odd";
+    }
+
+    return 0;
+}
